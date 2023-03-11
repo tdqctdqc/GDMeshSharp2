e9e30dc9e6d2243a7c641ca19e5fbfd43a05a852
= <=>
2023-03-10 19:04:13 -0500

    starting to parallelize generation

--- Error/SegmentsNotConnectedException.cs
+++ Error/SegmentsNotConnectedException.cs
@@ -4,16 +4,12 @@ using Godot;
 
 public class SegmentsNotConnectedException : DisplayableException
 {
-    public MapPolygon Poly { get; private set; }
     public List<LineSegment> SegsBefore { get; private set; }
     public List<LineSegment> SegsAfter { get; private set; }
     public List<List<LineSegment>> Partials { get; private set; }
-    public Data Data { get; private set; }
-    public SegmentsNotConnectedException(Data data, MapPolygon poly, List<LineSegment> segsBefore, List<LineSegment> segsAfter, 
+    public SegmentsNotConnectedException(List<LineSegment> segsBefore, List<LineSegment> segsAfter, 
         params List<LineSegment>[] partials)
     {
-        Data = data;
-        Poly = poly;
         SegsBefore = segsBefore;
         SegsAfter = segsAfter;
         Partials = partials.ToList();
